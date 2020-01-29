c112852379b6a84ad468002df8a9d62cca9e14c3
man4ish <mandecent.gupta@gmail.com>
2020-01-29 12:15:33 -0500

    fixed issue with build_featureset call

--- lib/GeneSet_Enrichment/Utils/featureSetBuilder.py
+++ lib/GeneSet_Enrichment/Utils/featureSetBuilder.py
@@ -12,36 +12,12 @@ from installed_clients.GenomeSearchUtilClient import GenomeSearchUtil
 from installed_clients.KBaseReportClient import KBaseReport
 from installed_clients.WorkspaceClient import Workspace as Workspace
 
-class FeatureSetBuilder:
+class featureSetBuilder:
 
-    def _mkdir_p(self, path):
-        """
-        _mkdir_p: make directory for given path
-        """
-        if not path:
-            return
-        try:
-            os.makedirs(path)
-        except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(path):
-                pass
-            else:
-                raise
-
-   
-    '''@staticmethod
-    def validate_params(params, expected, opt_param=set()):
-        """Validates that required parameters are present. Warns if unexpected parameters appear"""
-        expected = set(expected)
-        opt_param = set(opt_param)
-        pkeys = set(params)
-        if expected - pkeys:
-            raise ValueError("Required keys {} not in supplied parameters"
-                             .format(", ".join(expected - pkeys)))
-        defined_param = expected | opt_param
-        for param in params:
-            if param not in defined_param:
-                logging.warning("Unexpected parameter {} supplied".format(param))'''
+    def __init__(self, config):
+        self.ws_url = config["workspace-url"]
+        self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.dfu = DataFileUtil(self.callback_url)
 
     def _build_fs_obj(self, params):
         new_feature_set = {
@@ -49,7 +25,7 @@ class FeatureSetBuilder:
             'element_ordering': [],
             'elements': {}
         }
-        genome_ref = input_params['genome']
+        genome_ref = params['genome']
      
         element_ordering =[]
         elements = {}
@@ -63,34 +39,13 @@ class FeatureSetBuilder:
             elements[ids] = geno_ref
 
         new_feature_set = {
-            'description': input_params['description'],
+            'description': params['description'],
             'element_ordering': element_ordering,
             'elements': elements
         }
-
         return new_feature_set
 
-    def __init__(self, config):
-        self.ws_url = config["workspace-url"]
-        self.callback_url = config['SDK_CALLBACK_URL']
-        self.token = config['KB_AUTH_TOKEN']
-        self.shock_url = config['shock-url']
-        self.ws = Workspace(self.ws_url, token=self.token)
-        self.dfu = DataFileUtil(self.callback_url)
-        self.gsu = GenomeSearchUtil(self.callback_url)
-        self.scratch = config['scratch']
-
     def build_feature_set(self, params):
-        '''self.validate_params(params, {'output_feature_set', 'workspace_name', },
-                             {'genome', 'genes','description'})'''
-        
-        '''feature_sources = ('feature_ids', 'feature_ids_custom', 'base_feature_sets')
-        if not any([params.get(x) for x in feature_sources]):
-            raise ValueError("You must supply at least one feature source: {}".format(
-                ", ".join(feature_sources)))'''
-
-        gene_ids = re.split(r'[\r\n \n \t \s]', params['genes']) 
-        exit(gene_ids)
 
         workspace_id = self.dfu.ws_name_to_id(params['workspace_name'])
 
